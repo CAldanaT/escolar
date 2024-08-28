cce3bf21cd4cff3ebc9cc8d8e94529f3ba9b7c8e
Carlos Aldana <carlos.aldana@gmail.com>
2024-08-28 18:40:36 -0500

    comit initial

---
+++ escolarWeb/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Proforecast.Client.NewWebNgM';
+}
